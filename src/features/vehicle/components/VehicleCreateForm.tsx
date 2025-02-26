3eb1877ea38abdcfda94e4bfb830992f11ffc0fa
Luan de Carvalho <luan.carvalho@hpp.org.br>
2025-02-26 11:01:08 -0300

    feat: Adding select from shadcn and editing vehicle create form.

--- src/features/vehicle/components/VehicleCreateForm.tsx
+++ src/features/vehicle/components/VehicleCreateForm.tsx
@@ -17,9 +17,16 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { createVehicle } from "../api/create-vehicle";
 import { Vehicle } from "../types/vehicle";
 import { toast } from "sonner";
@@ -27,6 +34,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CirclePlus } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
+import { useVehicle } from "@/context/VehicleContext";
 
 const formSchema = z.object({
   plate: z.string(),
@@ -53,6 +61,16 @@ export default function CreateForm({
 }: {
   onVehicleCreated: () => void;
 }) {
+  const { colors } = useVehicle();
+  const { companies } = useVehicle();
+  const { fleets } = useVehicle();
+  const { fuels } = useVehicle();
+  const { marks } = useVehicle();
+  const { models } = useVehicle();
+  const { origins } = useVehicle();
+  const { status } = useVehicle();
+  const { subUnities } = useVehicle();
+  const { types } = useVehicle();
   const [isSheetOpen, setIsSheetOpen] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -129,6 +147,392 @@ export default function CreateForm({
                     </FormControl>
                     <FormDescription>
                       Este é o prefixo do seu veículo.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="tracker"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Rastreador
+                      <br />
+                    </FormLabel>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Isso define se o veículo possui rastreador.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="chassis"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Chassi</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                    <FormDescription>
+                      Este é o número de chassi do seu veículo.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="engine_number"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nº do Motor</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                    <FormDescription>
+                      Esta é a numeração do motor do seu veículo.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="renavam"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Renavam</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                    <FormDescription>
+                      Este é o número do renavam do seu veículo.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="year"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Ano</FormLabel>
+                    <FormControl>
+                      <Input {...field} required />
+                    </FormControl>
+                    <FormDescription>
+                      Este é o ano do seu veículo.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="color_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Cor</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione a cor do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {colors.map((color) => (
+                          <SelectItem key={color.id} value={color.id}>
+                            {color.color}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é a cor do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="company_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Orgão</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o orgão de origem do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {origins.map((origin) => (
+                          <SelectItem key={origin.id} value={origin.id}>
+                            {origin.origin}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é o orgão de origem do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="fleet_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Frota</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione a frota do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {fleets.map((fleet) => (
+                          <SelectItem key={fleet.id} value={fleet.id}>
+                            {fleet.fleet}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é a frota do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="fuel_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Combustível</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o combustível do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {fuels.map((fuel) => (
+                          <SelectItem key={fuel.id} value={fuel.id}>
+                            {fuel.fuel}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Este é o combustível do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="mark_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Marca</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione a marca do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {marks.map((mark) => (
+                          <SelectItem key={mark.id} value={mark.id}>
+                            {mark.mark}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é a marca do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="model_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Modelo</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o modelo do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {models.map((model) => (
+                          <SelectItem key={model.id} value={model.id}>
+                            {model.model}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Este é o modelo do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="origin_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Origem</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione a origem do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {origins.map((origin) => (
+                          <SelectItem key={origin.id} value={origin.id}>
+                            {origin.origin}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é a origem do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="status_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Status</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o status do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {status.map((status) => (
+                          <SelectItem key={status.id} value={status.id}>
+                            {status.status}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Este é o status do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="sub_unity_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Sub Unidade</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione a sub unidade do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {subUnities.map((subUnity) => (
+                          <SelectItem key={subUnity.id} value={subUnity.id}>
+                            {subUnity.sub_unity}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Esta é a sub unidade do seu veículo
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="type_id"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tipo</FormLabel>
+                    <Select
+                      onValueChange={(value) => field.onChange(value)}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Selecione o tipo do seu veículo" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {types.map((type) => (
+                          <SelectItem key={type.id} value={type.id}>
+                            {type.type}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      Este é o tipo do seu veículo
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
